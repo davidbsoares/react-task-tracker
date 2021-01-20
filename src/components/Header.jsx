a823775af179ce2451aec2bd40a915847c6d32c8
David Brum Soares <david.bsoares@hotmail.com>
2021-01-20 13:14:20 -0300

    Finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+import Button from './Button';
+
+// props = quaisquer parâmetros que serão usados no componente
+const Header = ({ title, onAdd, showAdd }) => {
+	const location = useLocation();
+
+	return (
+		<header className='header'>
+			<h1>{title}</h1>
+			{location.pathname === '/' && (
+				<Button
+					color={showAdd ? 'red' : 'green'}
+					text={showAdd ? 'Close' : 'Add'}
+					onClick={onAdd}
+				/>
+			)}
+		</header>
+	);
+};
+
+Header.defaultProps = {
+	title: 'Task Tracker',
+};
+
+// define tipos para os componentes
+Header.propTypes = {
+	title: PropTypes.string.isRequired,
+};
+
+export default Header;
